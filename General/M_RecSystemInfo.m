ba548231bd18027662a0ae0f2a30bd371c2dc09b
Diego Elgueda <delgueda@blues.isr.umd.edu>
2012-12-10 17:06:14 -0500

    Readded the mysteriously disappeared alpha omega system

--- General/M_RecSystemInfo.m
+++ General/M_RecSystemInfo.m
@@ -20,6 +20,9 @@ switch lower(SystemName)
     R = struct('Name',SystemName,'Gain',2000,'InputRange',[-5,5],'ChannelMap',[1:32],'Engine','NIDAQ');
   case 'am_systems_3000';
     R = struct('Name',SystemName,'Gain',10000,'InputRange',[-5,5],'ChannelMap',[1:32],'Engine','NIDAQ');
+  case 'alphaomega';
+    R = struct('Name',SystemName,'Gain',10000,'InputRange',[-5,5],'ChannelMap',[1:32],'Engine','AO');
+    
     
   % DIGITAL SYSTEMS
   case 'blackrock_96ch_16bit';
