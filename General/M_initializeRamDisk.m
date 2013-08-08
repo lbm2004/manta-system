ca3e27308e8620071ff31b63abcf24b8e124af6e
Bernhard Englitz <benglitz@gmail.com>
2013-08-08 11:57:57 +0200

    Automatize Initialization of RamDisk further

--- General/M_initializeRamDisk.m
+++ General/M_initializeRamDisk.m
@@ -6,5 +6,7 @@ global MG
 if ~exist(MG.DAQ.HSDIO.Path)
   Drive = MG.DAQ.HSDIO.Path(1:2);
   system(['imdisk -a -m ',Drive,' -t vm -s 500M -p "/fs:ntfs /q /y"']);
+  fprintf('Waiting for format to complete...\n');
+  while ~exist(Drive,'dir') pause(0.1); end
   mkdirAll(MG.DAQ.HSDIO.Path);
 end
